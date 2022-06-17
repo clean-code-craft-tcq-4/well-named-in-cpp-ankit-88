cfe63e70830b4b22e0f06b4892c41983907ef104
Ankit <ankitpandey2812@gmail.com>
2022-06-17 10:33:09 +0530

    main final edit

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main() {
 
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+	
+	Printf("\nColor Code  Manual \n\n");
 	TelCoColorCoder::printColorCodeManual();
 
     return 0;
